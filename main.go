e613318cd69f2a55db26ef06b5eb7af24cfc332a
zlowram <zlowram@gmail.com>
2015-02-17 23:57:16 +0100

    Created the main file
    - The main is now in charge to serve files and log into stderr

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	log.Println("Listening at 8080...")
+	log.Fatal(http.ListenAndServe(":8080", logger(http.FileServer(http.Dir("static")))))
+}
+
+func logger(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s - %s %s\n", r.RemoteAddr, r.Method, r.RequestURI)
+		handler.ServeHTTP(w, r)
+	})
+}
